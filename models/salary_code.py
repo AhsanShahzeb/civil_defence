ac116b1f8c32704082790e2ab348fc0b9c96e466
AhsanShahzeb <ahsanshahzebqureshi@gmail.com>
2023-12-19 19:53:03 +0500

    Some Fields and Some Files added

--- models/salary_code.py
+++ models/salary_code.py
@@ -4,8 +4,8 @@ from odoo import api, fields, models
 class CDSalaryCode(models.Model):
     _name = "cd.salary.code"
     _description = "Salary Code"
+    _rec_name = 'description'
 
     code = fields.Char("Salary Code", required=True)
     description = fields.Char("Salary Description", required=True)
     active = fields.Boolean("active", default=True)
-    source_id = fields.Many2one('cd.source.ii', string='Source')
--- models/sourceII.py
+++ models/sourceII.py
@@ -4,25 +4,21 @@ from odoo import api, fields, models
 class CDSourceII(models.Model):
     _name = "cd.source.ii"
     _description = "Source II"
+    _rec_name = 'name'
 
     employee_id = fields.Many2one('cd.employee', string='Employee', copy=False)
+    name = fields.Char(related='employee_id.name', readonly=True)
     personal = fields.Char(related='employee_id.personal')
     grade = fields.Char(related='employee_id.bps')
     dsignation = fields.Char(related='employee_id.designation_id.designation')
     cnic = fields.Char(related='employee_id.cnic')
-    salary_ids = fields.One2many('cd.salary.code', 'source_id', compute='_compute_salary_ids' ,string='Salary')
+    salaries_ids = fields.One2many('cd.source.salary', 'source_id', string='Select Salary Head')
 
 
-    @api.depends('employee_id')
-    def _compute_salary_ids(self):
-        for order in self:
-            order.salary_ids = order.employee_id.salary_ids
+class CDSourceSalary(models.Model):
+    _name = 'cd.source.salary'
+    _description = 'Source Salary'
 
-    # @api.depends('salary_ids')
-    # def _compute_salary_ids(self):
-    #     for i in self.salary_ids:
-
-    # @api.onchange('salary_ids')
-    # def _onchange_salary_ids(self):
-    #     for i in self.salary_ids:
-    #         i
+    salary_id = fields.Many2one('cd.salary.code', string='Salary')
+    amount = fields.Integer('Amount')
+    source_id = fields.Many2one('cd.source.ii', string='Source')
